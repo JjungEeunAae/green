404064adb0a03ba7735114c50bf6709a5bb7121d
EunAe <wjddmsdo77@gmail.com>
2024-01-04 18:10:12 +0900

    test

--- java_work/ex01/src/ex01/Programmers.java
+++ java_work/ex01/src/ex01/Programmers.java
@@ -1,22 +1,14 @@
 package ex01;
 
+import java.util.Scanner;
+
 public class Programmers {
 	public static void main(String[] args) {
-		int[] arr = {1, 2, 7, 10, 11};
-//		System.out.println(solution("aaaaa","bbbbb"));
-        
-	}
-	public static String solution(String str1, String str2) {
-        String answer = "";
-        
-        for(int i = 0 ; i < str1.length() ; i++) {
-        	char a = str1.charAt(i);
-        	char b = str2.charAt(i);
-        	
-        	answer += Character.toString(a) + Character.toString(b);
-        }
-        
-        
-        return answer;
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.printf("%d + %d = %d", a, b, (a+b));
     }
 }
