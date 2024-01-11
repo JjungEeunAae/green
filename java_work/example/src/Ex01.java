8ead998985bf489a610c34a4fa748825050f0d55
EunAe <wjddmsdo77@gmail.com>
2024-01-11 15:10:43 +0900

    한번 더 넣어본다

---
+++ java_work/example/src/Ex01.java
@@ -0,0 +1,7 @@
+public class Ex01 {
+    public static void main(String[] args) {
+        
+        // 이름을 String[] 배열안에 넣고 향상된 for구문으로 출력하세요.
+        String[] names = {"철수", "유리", "짱구", "맹구", "훈이"};
+    }
+}
